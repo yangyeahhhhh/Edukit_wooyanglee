15382fc50cf179c8bc94251f0c038aa7e306293b
daramjiyun <315ljy@gmail.com>
2022-12-07 14:17:41 +0900

    로그인 페이지 css2

--- wooyanglee_front/src/views/auth/login.vue
+++ wooyanglee_front/src/views/auth/login.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
-    <div style="margin-top: 500px">
-      <b-row align-h="center">
-        <b-col cols="4">
-          <b-card class="login">
-            <h3 align="center">로그인</h3>
-            <b-form-group label-cols="4" label-cols-lg="3" label="아이디" label-for="input-userid">
-              <b-form-input id="input-userid" v-model="userid"></b-form-input>
-            </b-form-group>
-            <b-form-group label-cols="4" label-cols-lg="3" label="패스워드" label-for="input-password">
-              <b-form-input id="input-password" v-model="password" type="password"></b-form-input>
-            </b-form-group>
-            <b-form-group label-cols="4" label-cols-lg="3" label="">
-              <b-button style="btn" :disabled="loading" @click="onSubmit"
-                ><b-spinner v-if="loading" small></b-spinner> 로그인</b-button
-              >
-            </b-form-group>
-            <div id="formFooter">
-              <div align="center" href="#">회원가입하기</div>
-            </div>
-          </b-card>
-        </b-col>
-      </b-row>
+    <div class="top"></div>
+    <div class="login-page" ng-app="">
+      <div class="login-content login-content-signin" ng-hide="showSignIn">
+        <div>
+          <h2>Log in</h2>
+          <form class="wrapper-box" role="form" ng-submit="login()">
+            <input v-model="userid" type="id" class="form-control form-control-email" placeholder="아이디" required />
+            <input
+              v-model="password"
+              type="password"
+              class="form-control form-control-password"
+              placeholder="Password"
+              required
+            />
+            <!-- <div class="checkbox pull-left">
+          <label>
+            <input type="checkbox"> Remember me.
+          </label>
+        </div> -->
+            <a class="outer-link pull-left" href="#/forgot">Forgot Password</a>
+            <button :disabled="loading" type="submit" @click="onSubmit">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -91,21 +93,177 @@ export default {
 </script>
 
 <style>
-body {
+/* /* top {
   background-image: url('~@/assets/images/1.jpg');
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   background-attachment: fixed;
+} */
+
+.login-page {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: #fff;
 }
-.login {
-  border-radius: 20px;
-  /* background-color: #D4ECDD; */
-  box-shadow: 8px 8px 5px #625a5a;
+.login-content {
+  width: 420px;
+  padding: 40px;
+  height: 500px;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  position: absolute;
+  background: #fff;
+  box-shadow: 0 0px 70px rgba(0, 0, 0, 0.1);
+  border-top: 5px solid #7ac142;
+  float: left;
+  -webkit-transition: all 0.2s ease-out;
+  transition: all 0.2s ease-out;
+  transition-delay: 0.2s;
 }
-.btn {
-  background-color: #d11541;
+.login-content-signin.ng-hide:not(.ng-hide-animate) {
+  transition-delay: 0s;
+  display: block !important;
+  opacity: 0;
+  z-index: -1;
+  -webkit-transform: translate(-45%, -50%);
+  transform: translate(-45%, -50%);
+}
+.login-content-signup.ng-hide:not(.ng-hide-animate) {
+  transition-delay: 0s;
+  display: block !important;
+  opacity: 0;
+  z-index: -1;
+  -webkit-transform: translate(-55%, -50%);
+  transform: translate(-55%, -50%);
+}
+h2 {
+  text-align: left;
+  color: #7ac142;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  margin: 0;
+  font-size: 18px;
+  font-weight: bold;
+}
+form.wrapper-box {
+  margin-top: 40px;
+}
+input {
+  margin-top: 20px;
   border: none;
-  background-position: center;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 0;
+  padding-left: 0;
+  box-shadow: none;
+  -webkit-transition: all 0.1s ease-out;
+  transition: all 0.1s ease-out;
+  /* &:valid { border-bottom: 1px solid #7ac142; } */
+  /* &:focus:invalid { border-bottom: 1px solid #EF6161; } */
+}
+/* ::-webkit-input-placeholder { color: rgba(0,0,0,0.5); }
+    :-moz-placeholder { color: rgba(0,0,0,0.5); }
+    ::-moz-placeholder { color: rgba(0,0,0,0.5); }
+    :-ms-input-placeholder { color: rgba(0,0,0,0.5); } */
+button {
+  display: inline-block;
+  margin-top: 50px;
+  border: 2px solid #7ac142;
+  background: #7ac142;
+  border-radius: 25px;
+  padding: 3px 12px 5px 12px;
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 0px;
+  -webkit-transition: all 0.1s ease-out;
+  transition: all 0.1s ease-out;
+}
+button:hover {
+  color: #7ac142;
+  background: #fff;
+  border: 2px solid #7ac142;
+}
+.outer-link {
+  display: inline-block;
+  margin-top: 50px;
+  padding: 5px 0;
+  display: block;
+  color: rgba(0, 0, 0, 0.4);
+  -webkit-transition: all 0.1s ease-out;
+  transition: all 0.1s ease-out;
+}
+outer-link:hover {
+  -webkit-transition: all 0s ease-out;
+  transition: all 0s ease-out;
+  color: rgba(0, 0, 0, 0.8);
+  text-decoration: none;
+}
+/* &.login-content-signin > div,
+  &.login-content-signup > div {
+    overflow: hidden;
+    top: 50%;
+    position: relative;
+    -webkit-transform: translateY(-50%);
+    transform: translateY(-50%);
+  }
+  &.login-content-signin { margin-left: -130px; }
+  &.login-content-signup { margin-left: 130px; } */
+.login-switcher {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 660px;
+  height: 400px;
+  box-shadow: 0 0px 20px rgba(0, 0, 0, 0.03);
+  /* & > div {
+    width: 40%;
+    margin-top: 125px;
+    &.login-switcher-signin { float: left; }
+    &.login-switcher-signup { float: right; } */
+}
+h3 {
+  color: rgba(0, 0, 0, 0.4);
+  text-align: center;
+  font-size: 14px;
+}
+button {
+  display: block;
+  margin: 20px auto 0 auto;
+  outline: 0;
+  background: none;
+  border: 2px solid rgba(0, 0, 0, 0.1);
+  border-radius: 20px;
+  color: rgba(0, 0, 0, 0.3);
+  font-weight: bold;
+  font-size: 14px;
+  padding: 4px 12px 5px 12px;
+  -webkit-transition: all 0.1s ease-out;
+  transition: all 0.1s ease-out;
+  /* &:hover {
+        border: 2px solid @green !important;
+        color: rgba(0,0,0,0.8);
+      } */
+}
+.login-switcher-signin,
+.login-switcher-signup {
+  -webkit-transition: all 0.1s ease-out;
+  transition: all 0.1s ease-out;
+}
+.login-switcher-signin.ng-hide:not(.ng-hide-animate),
+.login-switcher-signup.ng-hide:not(.ng-hide-animate) {
+  display: block !important;
+  opacity: 0;
+  -webkit-transform: translateY(10px);
+  transform: translateY(10px);
 }
 </style>
