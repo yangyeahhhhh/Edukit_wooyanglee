099bd30bf1af106e000a5b4541f3272723247e19
daramjiyun <315ljy@gmail.com>
2022-12-13 10:07:04 +0900

    작업내역 작업2

--- wooyanglee_front/src/views/department.vue
+++ wooyanglee_front/src/views/department.vue
@@ -1,57 +1,203 @@
 <template>
   <div>
-    <div class="separate-body"></div>
-    <app-sidebar />
-    <router-view />
-
-     <div class="row" style="padding: 30px">
-      <div class="col" style="padding: 30px">
-          <span class="span">시작날짜 <i class="bi bi-calendar3"></i></span>&nbsp;
-          <span class="span">끝날짜 <i class="bi bi-calendar3"></i></span>&nbsp;
-          <span class="span">조회 <i class="bi bi-search"></i></span>
-            <div class="row">
-              <div class="section col-2">번호</div>
-              <div class="section col-2">날짜</div>
-              <div class="section col-2">양품개수</div>
-              <div class="section col-2">불량품개수</div>
-              <div class="section col-2">총생산량</div>
-              <div class="section col-2">관리자</div>
-            </div>
-      </div>      
+    <h1>사용자 관리</h1>
+    <div style="margin-bottom: 5px">
+      <b-row>
+        <b-col style="text-align: left" cols="3">
+          <b-input-group class="mt-3">
+            <b-form-input v-model="search.name" placeholder="이름 검색"></b-form-input>
+            <b-form-input v-model="search.userid" placeholder="아이디 검색"></b-form-input>
+            <b-input-group-append>
+              <b-button variant="primary" size="sm" @click="searchUserList">검색</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-col>
+        <b-col style="text-align: right">
+          <b-button variant="success" size="sm" @click="onClickAddNew">신규등록</b-button>
+        </b-col>
+      </b-row>
     </div>
-  </div>
+    <div>
+      <b-table small hover striped :items="userList" :fields="fields">
+        <template #cell(Department)="row">
+          {{ row.item.Department && row.item.Department.name }}
+        </template>
+        <template #cell(createdAt)="row">
+          {{ row.item.createdAt.substring(0, 10) }}
+        </template>
+        <template #cell(updateBtn)="row">
+          <b-button size="sm" variant="success" @click="onClickEdit(row.item.id)">수정</b-button>
+        </template>
+        <template #cell(deleteBtn)="row">
+          <b-button size="sm" variant="danger" @click="onClickDelete(row.item.id)">삭제</b-button>
+        </template>
+      </b-table>
+    </div>
 
+    <!-- inform 영역 -->
+    <inform />
+  </div>
 </template>
 
 <script>
-import Sidebar from '../components/layout/Sidebar'
+import inform from './user/inform.vue'
 
 export default {
   components: {
-    'app-sidebar': Sidebar
+    inform: inform
+  },
+  data() {
+    return {
+      fields: [
+        { key: 'id', label: 'id' },
+        { key: 'name', label: '이름' },
+        { key: 'Department', label: '부서' },
+        { key: 'userid', label: '아이디' },
+        { key: 'role', label: '권한' },
+        { key: 'email', label: '이메일' },
+        { key: 'createdAt', label: '생성일' },
+        { key: 'updateBtn', label: '수정' },
+        { key: 'deleteBtn', label: '삭제' }
+      ],
+      search: {
+        name: null,
+        userid: null
+      }
+    }
+  },
+  computed: {
+    userList() {
+      return this.$store.getters.UserList
+    },
+    insertedResult() {
+      return this.$store.getters.UserInsertedResult
+    },
+    updatedResult() {
+      return this.$store.getters.UserUpdatedResult
+    },
+    deletedResult() {
+      return this.$store.getters.UserDeletedResult
+    }
+  },
+  watch: {
+    insertedResult(value) {
+      // 등록 후 처리
+
+      if (value !== null) {
+        if (value > 0) {
+          // 등록이 성공한 경우
+
+          // 1. 메세지 출력
+          this.$bvToast.toast('등록 되었습니다.', {
+            title: 'SUCCESS',
+            variant: 'success',
+            solid: true
+          })
+
+          // 2. 리스트 재 검색
+          this.searchUserList()
+        } else {
+          // 등록이 실패한 경우
+          this.$bvToast.toast('등록이 실패하였습니다.', {
+            title: 'ERROR',
+            variant: 'danger',
+            solid: true
+          })
+        }
+      }
+    },
+    updatedResult(value) {
+      // 수정 후 처리
+      if (value !== null) {
+        if (value > 0) {
+          // 수정이 성공한 경우
+
+          // 1. 메세지 출력
+          this.$bvToast.toast('수정 되었습니다.', {
+            title: 'SUCCESS',
+            variant: 'success',
+            solid: true
+          })
+
+          // 2. 리스트 재 검색
+          this.searchUserList()
+        } else {
+          // 수정이 실패한 경우
+          this.$bvToast.toast('수정이 실패하였습니다.', {
+            title: 'ERROR',
+            variant: 'danger',
+            solid: true
+          })
+        }
+      }
+    },
+    deletedResult(value) {
+      // 삭제 후 처리
+      if (value !== null) {
+        if (value > 0) {
+          // 삭제가 성공한 경우
+
+          // 1. 메세지 출력
+          this.$bvToast.toast('삭제 되었습니다.', {
+            title: 'SUCCESS',
+            variant: 'success',
+            solid: true
+          })
+
+          // 2. 리스트 재 검색
+          this.searchUserList()
+        } else {
+          // 삭제가 실패한 경우
+          this.$bvToast.toast('삭제가 실패하였습니다.', {
+            title: 'ERROR',
+            variant: 'danger',
+            solid: true
+          })
+        }
+      }
+    }
+  },
+  created() {
+    this.searchUserList()
+  },
+  methods: {
+    searchUserList() {
+      this.$store.dispatch('actUserList', this.search)
+    },
+    onClickAddNew() {
+      // 신규등록
+
+      // 1. 입력모드 설정
+      this.$store.dispatch('actUserInputMode', 'insert')
+
+      // 2. 상세정보 초기화
+      this.$store.dispatch('actUserInit')
+
+      // 3. 모달 출력
+      this.$bvModal.show('modal-user-inform')
+    },
+    onClickEdit(id) {
+      // (수정을 위한)상세정보
+
+      // 1. 입력모드 설정
+      this.$store.dispatch('actUserInputMode', 'update')
+
+      // 2. 상세정보 호출
+      this.$store.dispatch('actUserInfo', id)
+
+      // 3. 모달 출력
+      this.$bvModal.show('modal-user-inform')
+    },
+    onClickDelete(id) {
+      // 삭제
+      this.$bvModal.msgBoxConfirm('삭제 하시겠습니까?').then(value => {
+        if (value) {
+          this.$store.dispatch('actUserDelete', id)
+        }
+      })
+    }
   }
 }
 </script>
 
-<style scoped>
-.col{
-  text-align: right;
-  padding-top: 60px;
-  padding-right: 70px;
-}
-.span{
-  border: 3px solid #dc143c;
-  border-radius: 20px;
-  padding: 3px;
-}
-.section{
-  margin-top: 50px;
-  padding-left: 0px;
-  text-align: center;
-  border: 2px solid #388e3c;
-}
-.row2 {
-  text-align: center;
-  border: 1px solid #388e3c;
-}
-</style>
+<style lang="scss" scoped></style>
