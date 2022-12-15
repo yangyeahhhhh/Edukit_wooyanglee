23969662cd893928339ccebeb4b633c0143b78ba
yewon <ipunwon_@naver.com>
2022-12-15 18:16:14 +0900

    페이징 및 작업내역 날짜 조회 테이블

--- wooyanglee_front/src/components/chart/pieChart.js
+++ wooyanglee_front/src/components/chart/pieChart.js
@@ -1,22 +1,16 @@
 const config = {
   type: 'pie',
-  data: data,
-};
+  data: data
+}
 
 const data = {
-  labels: [
-    'Red',
-    'Blue',
-    'Yellow'
-  ],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
-  }]
-};
+  labels: ['Red', 'Blue', 'Yellow'],
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+      hoverOffset: 4
+    }
+  ]
+}
